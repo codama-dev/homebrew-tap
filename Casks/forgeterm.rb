8a0ee7b26fe2cf1e43b295d44778bd241c777e27
Nadav Cohen <ncamaa@gmail.com>
2026-07-27 13:33:10 +0200

    Update ForgeTerm to v0.22.0

--- Casks/forgeterm.rb
+++ Casks/forgeterm.rb
@@ -1,6 +1,6 @@
 cask "forgeterm" do
-  version "0.21.0"
-  sha256 "2d84c800fa0b5cf4be6b54b5916722a33032891def207a7162afe0bb0b035f7b"
+  version "0.22.0"
+  sha256 "51c9fc238d13713d2e70f27de2feccd668c31adf79813c8a70d55ee2011c0ede"
 
   url "https://github.com/ncamaa/forgeterm/releases/download/v#{version}/ForgeTerm-Mac-#{version}.dmg"
   name "ForgeTerm"
--- Formula/forgeterm-cli.rb
+++ Formula/forgeterm-cli.rb
@@ -1,9 +1,9 @@
 class ForgetermCli < Formula
   desc "CLI tool for ForgeTerm - open projects, send notifications, list recent projects"
   homepage "https://github.com/ncamaa/forgeterm"
-  url "https://github.com/ncamaa/forgeterm/releases/download/v0.21.0/forgeterm-cli.sh"
+  url "https://github.com/ncamaa/forgeterm/releases/download/v0.22.0/forgeterm-cli.sh"
   sha256 "c0a0f53c0b898c9c68af7ab55c27d1e8e2e8651dc29a6bd07259ead5d054eadb"
-  version "0.21.0"
+  version "0.22.0"
   license "MIT"
 
   def install
